7c02504c0d7a6abc5c850713aa1a7dbd7bfdebef
Minoo Sayyadpour <minoosayyadpour3@gmail.com>
2025-05-07 15:14:49 +0330

    Update Caesar cipher.py

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -1,46 +1,99 @@
-import string
+def caesar_encryption(number, text):
+  """Encrypts a string using Caesar cipher, shifting all characters.
 
-def caesar_cipher(text, key, mode):
-    """
-    Encrypts or decrypts a message using the Caesar cipher algorithm.
+  This function shifts each character in the input text by the specified
+  number, wrapping around the ASCII table. It does not handle alphabetic
+  characters differently.
 
-    Parameters:
-        text (str): The input message to be encrypted or decrypted.
-        key (int): The number of positions to shift each letter in the alphabet.
-        mode (str): The mode of operation, either 'encrypt' or 'decrypt'.
+  Args:
+    number (int): The shift value for the cipher.
+    text (str): The text to encrypt.
 
-    Returns:
-        str: The transformed message after encryption or decryption.
-    """
-    alphabet = string.ascii_lowercase
-    result = ''
+  Returns:
+    str: The encrypted text.
+  """
+  output = ''
+  for i in text:
+    output += chr(ord(i) + number)
+  return output
 
-    key = key % 26
+def caesar_decryption(number, enc_text):
+  """Decrypts a string encrypted with Caesar cipher, shifting all characters.
 
-    if mode == 'decrypt':
-        key = -key
+  This function shifts each character in the encrypted text back by the
+  specified number, wrapping around the ASCII table. It does not handle
+  alphabetic characters differently.
 
-    for char in text:
-        if char.lower() in alphabet:
-            original_index = alphabet.find(char.lower())
-            new_index = (original_index + key) % 26
-            if char.isupper():
-                result += alphabet[new_index].upper()
-            else:
-                result += alphabet[new_index]
-        else:
-            result += char
+  Args:
+    number (int): The shift value for the cipher.
+    enc_text (str): The encrypted text.
 
-    return result
+  Returns:
+    str: The decrypted text.
+  """
+  output = ''
+  for i in enc_text:
+    output += chr(ord(i) - number)
+  return output
+
+def caesar_encryption_only_alpha(number, text):
+ """Encrypts a string using Caesar cipher, only for alphabet characters.
+
+ Args:
+  number (int): The shift value for the cipher.
+  text (str): The text to encrypt.
+
+ Returns:
+  str: The encrypted text.
+ """
+ output = ''
+ for i in text:
+  if i.isalpha():
+   if i.isupper():
+    output += chr(((ord(i) + number - 65) % 26) + 65) 
+   else:
+    output += chr(((ord(i) + number - 97) % 26) + 97)
+  else:
+   output += i
+ return output
+
+def caesar_decryption_only_alpha(number, text):
+ """Decrypts a string encrypted with Caesar cipher, only for alphabet characters.
+
+ Args:
+  number (int): The shift value for the cipher.
+  text (str): The encrypted text.
+
+ Returns:
+  str: The decrypted text.
+ """
+ output = ''
+ for i in text:
+  if i.isalpha(): 
+   if i.isupper(): 
+    output += chr(((ord(i) - number - 65) % 26) + 65)
+   else: 
+    output += chr(((ord(i) - number - 97) % 26) + 97)
+  else: 
+   output += i
+ return output
 
 
 
-text = input("Enter the message: ")
-key = int(input("Enter the key (shift value): "))
-mode = input("Choose mode ('encrypt' or 'decrypt'): ").lower()
-
-if mode not in ['encrypt', 'decrypt']:
-    print("Invalid mode. Please choose 'encrypt' or 'decrypt'.")
-else:
-    transformed_text = caesar_cipher(text, key, mode)
-    print(f"Result: {transformed_text}")
+while True:
+  try:
+    choice = int(input('Enter your choice:\n1: Encrypt\n2: Decrypt\n3: Exit\n'))
+    match choice:
+      case 1:
+        text = input('Enter your simple text: ')
+        encrypt_number = int(input('Enter the encrypt code: '))
+        print(caesar_encryption_only_alpha(encrypt_number,text))
+      case 2:
+        text = input('Enter your encrypted text: ')
+        decrypt_number = int(input('Enter the decrypt code: '))
+        print(caesar_decryption_only_alpha(decrypt_number,text))
+      case 3:
+        break
+  except:
+    print(chr(27) + "[2J")
+    print('You enter a wrong input. Try again.')
